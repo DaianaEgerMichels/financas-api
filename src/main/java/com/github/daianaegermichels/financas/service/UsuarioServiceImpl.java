0c199af01832214aa9470d5b2b36420243e1bf3a
Daiana Michels <daianaem.12@gmail.com>
2022-06-22 11:59:38 -0300

    feat: inicia classe service de usuario

--- src/main/java/com/github/daianaegermichels/financas/service/UsuarioServiceImpl.java
+++ src/main/java/com/github/daianaegermichels/financas/service/UsuarioServiceImpl.java
@@ -1,2 +1,31 @@
-package com.github.daianaegermichels.financas.service;public class UsuarioServiceImpl {
+package com.github.daianaegermichels.financas.service;
+
+import com.github.daianaegermichels.financas.model.Usuario;
+import com.github.daianaegermichels.financas.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService{
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioServiceImpl(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public Usuario autenticar(String email, String senha) {
+        return null;
+    }
+
+    @Override
+    public Usuario salvarUsuario(Usuario usuario) {
+        return null;
+    }
+
+    @Override
+    public void validarEmail(String email) {
+
+    }
 }
