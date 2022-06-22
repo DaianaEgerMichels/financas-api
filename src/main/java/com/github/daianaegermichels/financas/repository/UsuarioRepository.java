50a36a7831d61315b8570e93f3c2ded954379bf0
Daiana Michels <daianaem.12@gmail.com>
2022-06-21 21:51:36 -0300

    feat: cria repositories

--- src/main/java/com/github/daianaegermichels/financas/repository/UsuarioRepository.java
+++ src/main/java/com/github/daianaegermichels/financas/repository/UsuarioRepository.java
@@ -1,2 +1,9 @@
-package com.github.daianaegermichels.financas.repository;public interface UsuarioRepository {
+package com.github.daianaegermichels.financas.repository;
+
+import com.github.daianaegermichels.financas.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 }
