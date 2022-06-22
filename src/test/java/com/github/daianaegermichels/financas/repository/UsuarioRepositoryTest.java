9c4d5b5dd1688bc96029a5f3319abc68a37efad3
Daiana Michels <daianaem.12@gmail.com>
2022-06-22 15:18:38 -0300

    feat: adiciona classe de teste

--- src/test/java/com/github/daianaegermichels/financas/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/github/daianaegermichels/financas/repository/UsuarioRepositoryTest.java
@@ -1,5 +1,30 @@
 package com.github.daianaegermichels.financas.repository;
 
-public class UsuarioRepository {
-    
+import com.github.daianaegermichels.financas.model.Usuario;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+public class UsuarioRepositoryTest {
+
+    @Autowired
+    UsuarioRepository repository;
+
+    @Test
+    public void deveVerificarAExistenciaDeUmEmail(){
+        //cenário
+        var usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
+        repository.save(usuario);
+        //execução
+        boolean result = repository.existsByEmail("usuario@email.com");
+        //verificação
+        Assertions.assertThat(result).isTrue();
+    }
 }
