e62cc9e1d6746743db0e66f82aaafa2622d4721b
Daiana Michels <daianaem.12@gmail.com>
2022-06-23 13:53:10 -0300

    test: altera classes de test

--- src/test/java/com/github/daianaegermichels/financas/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/github/daianaegermichels/financas/repository/UsuarioRepositoryTest.java
@@ -5,26 +5,40 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
-@ExtendWith(SpringExtension.class)
+
 @ActiveProfiles("test")
 public class UsuarioRepositoryTest {
 
     @Autowired
     UsuarioRepository repository;
 
+    @Autowired
+    TestEntityManager entityManager;
+
     @Test
     public void deveVerificarAExistenciaDeUmEmail(){
         //cenário
-        var usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
-        repository.save(usuario);
+        var usuario = criarUsuario();
+        entityManager.persist(usuario);
+        //repository.save(usuario);
         //execução
         boolean result = repository.existsByEmail("usuario@email.com");
         //verificação
         Assertions.assertThat(result).isTrue();
+    }
+
+    public static Usuario criarUsuario() {
+        return Usuario
+                .builder()
+                .nome("usuario")
+                .email("usuario@email.com")
+                .senha("senha")
+                .build();
     }
 }
