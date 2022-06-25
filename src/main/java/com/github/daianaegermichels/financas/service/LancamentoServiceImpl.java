4ee77ae0ba0430c46688866a9b23cbe75c832b00
Daiana Michels <daianaem.12@gmail.com>
2022-06-25 16:04:21 -0300

    feat: adiciona método para validar o lançamento

--- src/main/java/com/github/daianaegermichels/financas/service/LancamentoServiceImpl.java
+++ src/main/java/com/github/daianaegermichels/financas/service/LancamentoServiceImpl.java
@@ -1,6 +1,7 @@
 package com.github.daianaegermichels.financas.service;
 
 import com.github.daianaegermichels.financas.enuns.StatusLancamento;
+import com.github.daianaegermichels.financas.exception.RegraNegocioException;
 import com.github.daianaegermichels.financas.model.Lancamento;
 import com.github.daianaegermichels.financas.repository.LancamentoRepository;
 import org.springframework.data.domain.Example;
@@ -8,6 +9,7 @@ import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 
@@ -23,6 +25,8 @@ public class LancamentoServiceImpl implements LancamentoService{
     @Override
     @Transactional
     public Lancamento salvar(Lancamento lancamento) {
+        validar(lancamento);
+        lancamento.setStatus(StatusLancamento.PENDENTE);
         return repository.save(lancamento);
     }
 
@@ -30,6 +34,7 @@ public class LancamentoServiceImpl implements LancamentoService{
     @Transactional
     public Lancamento atualizar(Lancamento lancamento) {
         Objects.requireNonNull(lancamento.getId());
+        validar(lancamento);
         return repository.save(lancamento);
     }
 
@@ -54,5 +59,32 @@ public class LancamentoServiceImpl implements LancamentoService{
     @Override
     public void atualizarStatus(Lancamento lancamento, StatusLancamento status) {
 
+    }
+
+    @Override
+    public void validar(Lancamento lancamento) {
+        if(lancamento.getDescricao() == null || lancamento.getDescricao().trim().equals("")){
+            throw new RegraNegocioException("Informe uma Descrição válida!")
+        }
+
+        if(lancamento.getMes() == null || lancamento.getMes() < 1 || lancamento.getMes() > 12){
+            throw new RegraNegocioException("Informe um Mês válido!");
+        }
+
+        if(lancamento.getAno() == null || lancamento.getAno().toString().length() !=4){
+            throw new RegraNegocioException("Informe um Ano válido!");
+        }
+
+        if(lancamento.getUsuario() == null || lancamento.getUsuario().getId() == null){
+            throw new RegraNegocioException("Informe um Usuário!");
+        }
+
+        if(lancamento.getValor() == null || lancamento.getValor().compareTo(BigDecimal.ZERO) < 1){
+            throw new RegraNegocioException("Informe um Valor válido!");
+        }
+
+        if(lancamento.getTipo() == null){
+            throw new RegraNegocioException("Informe um Tipo de Lançamento!");
+        }
     }
 }
