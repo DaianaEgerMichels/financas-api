ca4c6539c83a396629d71b7c631f1d320caf4974
Daiana Michels <daianaem.12@gmail.com>
2022-06-28 14:42:12 -0300

    test: adiciona classe para testes dos metodos de LancamentoService

---
+++ src/test/java/com/github/daianaegermichels/financas/service/LancamentoServiceTest.java
@@ -0,0 +1,12 @@
+package com.github.daianaegermichels.financas.service;
+
+import org.mockito.Mock;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("Test")
+public class LancamentoServiceTest {
+
+    @Mock
+    LancamentoService service;
+
+}
