133b67bb8259da91e656b92b59730c499a076472
Daiana Michels <daianaem.12@gmail.com>
2022-06-29 15:47:07 -0300

    test: implementa testes para endpoint salvarUsuario - controller

--- src/test/java/com/github/daianaegermichels/financas/controller/UsuarioControllerTest.java
+++ src/test/java/com/github/daianaegermichels/financas/controller/UsuarioControllerTest.java
@@ -1,17 +1,16 @@
 package com.github.daianaegermichels.financas.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.daianaegermichels.financas.api.controller.UsuarioController;
 import com.github.daianaegermichels.financas.dto.UsuarioDTO;
 import com.github.daianaegermichels.financas.exception.ErroAutenticacao;
+import com.github.daianaegermichels.financas.exception.RegraNegocioException;
 import com.github.daianaegermichels.financas.model.Usuario;
-import com.github.daianaegermichels.financas.repository.UsuarioRepository;
 import com.github.daianaegermichels.financas.service.LancamentoService;
 import com.github.daianaegermichels.financas.service.UsuarioService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -23,6 +22,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(SpringExtension.class)
@@ -37,9 +37,6 @@ public class UsuarioControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-//    @MockBean
-//    private UsuarioRepository usuarioRepository;
-
     @MockBean
     private UsuarioService usuarioService;
 
@@ -47,6 +44,7 @@ public class UsuarioControllerTest {
     private LancamentoService lancamentoService;
 
     @Test
+    @DisplayName("Autenticação usuário - ok")
     public void deveAutenticarUmUsuario() throws Exception {
         //cenario
         String email = "usuario@email.com";
@@ -75,6 +73,7 @@ public class UsuarioControllerTest {
     }
 
     @Test
+    @DisplayName("Autenticação usuário - bad request")
     public void deveRetornarBadRequestAoObterErroDeAutenticacao() throws Exception {
         //cenario
         String email = "usuario@email.com";
@@ -89,6 +88,60 @@ public class UsuarioControllerTest {
 
         //execução e verificação
         var request = MockMvcRequestBuilders.post(API.concat("/autenticar"))
+                .accept(JSON)
+                .contentType(JSON)
+                .content(json);
+
+        mockMvc
+                .perform(request)
+                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Salvar usuário - created")
+    public void deveSalvarUmUsuario() throws Exception {
+        //cenario
+        String email = "usuario@email.com";
+        String senha = "123";
+        String nome = "usuario";
+
+        var dto = UsuarioDTO.builder().email(email).senha(senha).nome(nome).build();
+        var usuario = Usuario.builder().id(1l).email(email).senha(senha).nome(nome).build();
+
+        when(usuarioService.salvarUsuario(any(Usuario.class))).thenReturn(usuario);
+
+        var json = new ObjectMapper().writeValueAsString(dto);
+
+        //execução e verificação
+        var request = MockMvcRequestBuilders.post(API)
+                .accept(JSON)
+                .contentType(JSON)
+                .content(json);
+
+        mockMvc
+                .perform(request)
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("id").value(usuario.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("nome").value(usuario.getNome()))
+                .andExpect(MockMvcResultMatchers.jsonPath("email").value(usuario.getEmail()));
+    }
+
+    @Test
+    @DisplayName("Salvar usuário - bad request")
+    public void naoDeveSalvarUmUsuario() throws Exception {
+        //cenario
+        String email = "usuario@email.com";
+        String senha = "123";
+        String nome = "usuario";
+
+        var dto = UsuarioDTO.builder().email(email).senha(senha).nome(nome).build();
+
+        when(usuarioService.salvarUsuario(any(Usuario.class))).thenThrow(RegraNegocioException.class);
+
+        var json = new ObjectMapper().writeValueAsString(dto);
+
+        //execução e verificação
+        var request = MockMvcRequestBuilders.post(API)
                 .accept(JSON)
                 .contentType(JSON)
                 .content(json);
