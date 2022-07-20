2ed650236d077b029935a0b1d30813fe306b415d
Daiana Michels <daianaem.12@gmail.com>
2022-07-19 21:30:20 -0300

    fix: corrige query

--- src/main/java/com/github/daianaegermichels/financas/repository/LancamentoRepository.java
+++ src/main/java/com/github/daianaegermichels/financas/repository/LancamentoRepository.java
@@ -13,7 +13,7 @@ import java.math.BigDecimal;
 @Repository
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 
-    @Query(value = " select sum(l.valor) from Lancamento l join l.usuario u where u.id = :idUsuario and l.tipo = :tipo group by u ", nativeQuery = true)
+    @Query(value = " select sum(l.valor) from Lancamento l join l.usuario u where u.id = :idUsuario and l.tipo = :tipo group by u ")
     BigDecimal obterSaldoPorTipoDeLancamentoEUsuario(@Param("idUsuario") Long idUsuario, @Param("tipo") TipoLancamento tipo);
 
 }
