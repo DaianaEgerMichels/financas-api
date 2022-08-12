d823dd1c98be539af527ed75c8248a48975db880
Daiana Michels <daianaem.12@gmail.com>
2022-08-11 21:14:08 -0300

    feat: adiciona encoder para codificação das senhas

--- src/main/java/com/github/daianaegermichels/financas/config/SecurityConfig.java
+++ src/main/java/com/github/daianaegermichels/financas/config/SecurityConfig.java
@@ -1,18 +1,33 @@
 package com.github.daianaegermichels.financas.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        PasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
+
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        super.configure(auth);
+        String senhaCodificada = passwordEncoder().encode("qwe123");
+
+        auth.inMemoryAuthentication()
+                .withUser("usuario")
+                .password(senhaCodificada)
+                .roles("USER");
     }
 
     @Override
