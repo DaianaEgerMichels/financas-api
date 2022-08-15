57a4fcb0f84b6cafd30bafc22b05be49fc82c8b8
Daiana Michels <daianaem.12@gmail.com>
2022-08-15 15:29:39 -0300

    feat: finaliza autenticação Basic

--- src/main/java/com/github/daianaegermichels/financas/service/SecurityUserDetailsService.java
+++ src/main/java/com/github/daianaegermichels/financas/service/SecurityUserDetailsService.java
@@ -1,5 +1,7 @@
 package com.github.daianaegermichels.financas.service;
 
+import com.github.daianaegermichels.financas.repository.UsuarioRepository;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -8,8 +10,21 @@ import org.springframework.stereotype.Service;
 @Service
 public class SecurityUserDetailsService implements UserDetailsService {
 
+    private UsuarioRepository usuarioRepository;
+
+    public SecurityUserDetailsService(UsuarioRepository usuarioRepository){
+        this.usuarioRepository = usuarioRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return null;
+        var usuarioEncontrado = usuarioRepository.findByEmail(email)
+                .orElseThrow(()-> new UsernameNotFoundException("Email não cadastrado!"));
+
+        return User.builder()
+                .username(usuarioEncontrado.getEmail())
+                .password(usuarioEncontrado.getSenha())
+                .roles("USER")
+                .build();
     }
 }
